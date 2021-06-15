c6eeaa34e4fd78dcd4587c69f06dc43beb6b9363
Damián Ricobelli <damian.ricobelli@outlook.es>
2021-06-15 16:57:24 -0300

    Setup chakra ui and other dependencies

---
+++ src/styles/theme.tsx
@@ -0,0 +1,21 @@
+import { extendTheme, useColorModeValue } from "@chakra-ui/react";
+
+const styles = {
+  global: () => ({
+    body: {
+      bg: useColorModeValue("gray.50", "gray.800"),
+    },
+  }),
+};
+
+const theme = extendTheme({
+  styles,
+  initialColorMode: "dark",
+  useSystemColorMode: false,
+  fonts: {
+    body: "'Poppins', sans-serif",
+    heading: "'Poppins', sans-serif",
+  },
+});
+
+export default theme;
